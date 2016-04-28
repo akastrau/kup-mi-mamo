03eb6d3c46888556350d58f6b0736584ffc09da3
Adrian Kastrau <akastrau@outlook.com>
2016-04-28 12:57:10 +0200

    small changes

--- app/controllers/new.js
+++ app/controllers/new.js
@@ -3,7 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     sortProperties: ['timestamp'],
     sortAscending: true, // sorts vehicles by timestamp
-    noErrors: false,
+    noErrors: null,
+    noFilledForm: null,
     actions: {
         publishVehicle : function() {
         var newVehicle = this.store.createRecord('vehicle', 
@@ -13,9 +14,9 @@ export default Ember.Controller.extend({
             timestamp: new Date().getTime()
         });
         
-        if (this.get('place') === "" || this.get('mark') === "" || this.get('place') === undefined || this.get('mark') === undefined)
+        if (this.get('place') == "" || this.get('mark') == "" || this.get('place') == undefined || this.get('mark') == undefined)
         {
-             this.set("noErrors", false);
+             this.set("noFilledForm", true);
         }
         else 
         {
--- app/routes/new.js
+++ app/routes/new.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    actions: {
+          willTransition: function(transition) {
+            this.controller.set('noErrors').clear();
+            this.controller.set('noFilledForm').clear();
+          }
+        },
     model : function() 
     {
        
