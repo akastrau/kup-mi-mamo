c51507acf682f979a5963043de6386a86aae6c81
Adrian Kastrau <akastrau@outlook.com>
2016-04-30 16:26:20 +0200

    fix in list.js

--- app/routes/list.js
+++ app/routes/list.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    
-    
-    actions: 
-    {
-        loading: function () {
-            alert("Loading...  ");
-        },
-    }
     model : function() {
         return this.store.findAll('vehicle');
     }
